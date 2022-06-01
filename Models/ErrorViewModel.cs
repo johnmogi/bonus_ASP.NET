1c65b1b485d0b3938809eee5f6d256e2414cb942
johnmogi <anguru@gmail.com>
2022-06-01 21:50:35 +0300

    start

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace my_first_asp_.net_core_web_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
